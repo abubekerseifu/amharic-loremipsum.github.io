a7a8e14fee1bbac54245228acad17704d5987fa4
Abubeker Seifu <abubeker3522@gmail.com>
2020-06-15 17:07:05 +0300

    Initial build subtree commit

---
+++ precache-manifest.976033b0d47dc279c966cf84a48fd59a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4092aa28d80565155e508bad5b3d153d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "16b599c969ea0da1d303",
+    "url": "./static/js/2.a65b4ab9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a65b4ab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ecddc1ea7bc069b56e",
+    "url": "./static/js/main.c605cc01.chunk.js"
+  },
+  {
+    "revision": "321f804a1fb53d5cde17",
+    "url": "./static/js/runtime-main.05c4aa70.js"
+  }
+]);
