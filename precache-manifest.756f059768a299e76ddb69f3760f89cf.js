a2bd05ae3b9e539f968568ddf8cd68d5c0e918bd
Abubeker Seifu <abubeker3522@gmail.com>
2020-06-15 12:48:32 +0300

    index.html

---
+++ precache-manifest.756f059768a299e76ddb69f3760f89cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88058ae1cec0afc31c738dfa5234550",
+    "url": "/index.html"
+  },
+  {
+    "revision": "414585173ed39f60f9e4",
+    "url": "/static/css/main.f45226ac.chunk.css"
+  },
+  {
+    "revision": "16b599c969ea0da1d303",
+    "url": "/static/js/2.a65b4ab9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a65b4ab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "414585173ed39f60f9e4",
+    "url": "/static/js/main.59299c92.chunk.js"
+  },
+  {
+    "revision": "bcc4dddf23a2c5fcdec7",
+    "url": "/static/js/runtime-main.7537757d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
